2bb5d64cd101be15c570a87d2cf308e197297c6f
parthwps <parth@netdevs.net>
2024-12-15 23:39:59 +0530

    update

--- dealer_portal/vendor/mollie/mollie-api-php/src/Endpoints/PaymentRefundEndpoint.php
+++ dealer_portal/vendor/mollie/mollie-api-php/src/Endpoints/PaymentRefundEndpoint.php
@@ -2,6 +2,7 @@
 
 namespace Mollie\Api\Endpoints;
 
+use Mollie\Api\Resources\LazyCollection;
 use Mollie\Api\Resources\Payment;
 use Mollie\Api\Resources\Refund;
 use Mollie\Api\Resources\RefundCollection;
@@ -74,6 +75,22 @@ class PaymentRefundEndpoint extends CollectionEndpointAbstract
     }
 
     /**
+     * Create an iterator for iterating over refunds for the given payment, retrieved from Mollie.
+     *
+     * @param Payment $payment
+     * @param string|null $from The first resource ID you want to include in your list.
+     * @param int|null $limit
+     * @param array $parameters
+     * @param bool $iterateBackwards Set to true for reverse order iteration (default is false).
+     *
+     * @return LazyCollection
+     */
+    public function iteratorFor(Payment $payment, ?string $from = null, ?int $limit = null, array $parameters = [], bool $iterateBackwards = false): LazyCollection
+    {
+        return $this->iteratorForId($payment->id, $from, $limit, $parameters, $iterateBackwards);
+    }
+
+    /**
      * @param string $paymentId
      * @param array $parameters
      *
@@ -85,6 +102,24 @@ class PaymentRefundEndpoint extends CollectionEndpointAbstract
         $this->parentId = $paymentId;
 
         return parent::rest_list(null, null, $parameters);
+    }
+
+    /**
+     * Create an iterator for iterating over refunds for the given payment id, retrieved from Mollie.
+     *
+     * @param string $paymentId
+     * @param string|null $from The first resource ID you want to include in your list.
+     * @param int|null $limit
+     * @param array $parameters
+     * @param bool $iterateBackwards Set to true for reverse order iteration (default is false).
+     *
+     * @return LazyCollection
+     */
+    public function iteratorForId(string $paymentId, ?string $from = null, ?int $limit = null, array $parameters = [], bool $iterateBackwards = false): LazyCollection
+    {
+        $this->parentId = $paymentId;
+
+        return $this->rest_iterator($from, $limit, $parameters, $iterateBackwards);
     }
 
 
@@ -118,5 +153,48 @@ class PaymentRefundEndpoint extends CollectionEndpointAbstract
         $this->parentId = $paymentId;
 
         return parent::rest_create($data, $filters);
+    }
+
+    /**
+     * @param \Mollie\Api\Resources\Payment $payment
+     * @param string $refundId
+     * @param array $parameters
+     * @return null
+     *
+     * @throws \Mollie\Api\Exceptions\ApiException
+     */
+    public function cancelForPayment(Payment $payment, string $refundId, array $parameters = [])
+    {
+        $this->parentId = $payment->id;
+
+        return $this->cancelForId($payment->id, $refundId, $parameters);
+    }
+
+    /**
+     * @param string $paymentId
+     * @param string $refundId
+     * @param array $parameters
+     * @return null
+     *
+     * @throws \Mollie\Api\Exceptions\ApiException
+     */
+    public function cancelForId(string $paymentId, string $refundId, array $parameters = [])
+    {
+        $this->parentId = $paymentId;
+
+        $body = null;
+        if (count($parameters) > 0) {
+            $body = json_encode($parameters);
+        }
+
+        $this->client->performHttpCall(
+            EndpointAbstract::REST_DELETE,
+            $this->getResourcePath() . '/' . $refundId,
+            $body
+        );
+
+        $this->getResourcePath();
+
+        return null;
     }
 }
